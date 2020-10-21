d141bb1bb0b4039f9c08c4dbf547a8c609b767a3
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2020-10-20 23:36:30 -0300

    Use subject for instance

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,19 +1,21 @@
 require 'calculator'
 
 describe Calculator do
+  # Para criar uma instancia com um nome personalizado
+  #   subject(:calc) { described_class.new() }
+
   # Context serve para agrupar
   context '#sum' do
     # Que teste vai ser feito dentro do metodo?
     it 'with positive numbers' do
-      calc = Calculator.new
-      result = calc.sum(5, 7)
+      # Subject faz uma instancia da classe automaticamente
+      result = subject.sum(5, 7)
       expect(result).to eq(12)
     end
 
     # Pode usar it, specify ou example
-    it 'wit negative numbers' do
-      calc = Calculator.new
-      result = calc.sum(-5, 7)
+    it 'wit negative numbers' do      
+      result = subject.sum(-5, 7)
       expect(result).to eq(2)
     end
   end
