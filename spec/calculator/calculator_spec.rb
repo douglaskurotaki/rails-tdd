97ac9d44659096b0172c382ee5c3db3c44267cd3
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2020-10-20 23:31:39 -0300

    Context (group)

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,18 +1,21 @@
 require 'calculator'
 
 describe Calculator do
-  # Que teste vai ser feito?
-  it 'use sum method for 2 numbers' do
-    calc = Calculator.new
-    result = calc.sum(5, 7)
-    expect(result).to eq(12)
-  end
+  # Context serve para agrupar
+  context '#sum' do
+    # Que teste vai ser feito dentro do metodo?
+    it 'with positive numbers' do
+      calc = Calculator.new
+      result = calc.sum(5, 7)
+      expect(result).to eq(12)
+    end
 
-  # Pode usar it, specify ou example
-  specify 'use sum method for 2 numbers with negative numbers' do
-    calc = Calculator.new
-    result = calc.sum(-5, 7)
-    expect(result).to eq(2)
+    # Pode usar it, specify ou example
+    it 'wit negative numbers' do
+      calc = Calculator.new
+      result = calc.sum(-5, 7)
+      expect(result).to eq(2)
+    end
   end
 
   # Testes devem ser confiaveis, faceis de escrever e nao estar focado em velocidade
