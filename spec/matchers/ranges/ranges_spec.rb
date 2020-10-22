248f7286db546558396a52a1fb0bdc4e4efd81f9
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2020-10-21 23:55:46 -0300

    One line sintax

--- spec/matchers/ranges/ranges_spec.rb
+++ spec/matchers/ranges/ranges_spec.rb
@@ -4,5 +4,16 @@ describe (1..5), 'Ranges' do
     expect(subject).to cover(2)
     expect(subject).to cover(2, 5)
     expect(subject).not_to cover(0, 6)
+
+    # Substitui o expect(subject).to
+    is_expected.to cover(2)
   end
+
+  # Simplifica o it com o subject
+  it { is_expected.to cover(2) }
+  it { is_expected.to cover(2, 5) }
+  it { is_expected.not_to cover(0, 6) }
+
+  # Tambem pode utilizar a forma normal do subject
+  it { expect(subject).to cover(2, 5) }
 end
