176640637eebc4188d3653273d200fa29dc7e622
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2020-10-22 00:30:38 -0300

    Helpers module

--- spec/composicao/composicao_spec.rb
+++ spec/composicao/composicao_spec.rb
@@ -1,8 +1,16 @@
+# require_relative 'helper'
+
+# Alem do require eh necessaro incluir a classe dessa forma
+# RSpec.configure do |conf|
+#   conf.include Helper
+# end
+
 describe 'Ruby on Rails' do
   it { is_expected.to start_with('Ruby').and end_with('Rails') }
   it { expect(frutas).to eq('banana').or eq('laranja').or eq('uva') }
 
-  def frutas
-    %w[banana laranja uva].sample
-  end
+  # helper method arbitrario
+  # def frutas
+  #   %w[banana laranja uva].sample
+  # end
 end
