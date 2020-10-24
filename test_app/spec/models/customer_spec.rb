723621d2f9cb5f2af54762ebcf36abb13bb52a2b
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2020-10-24 17:13:10 -0300

    Rails - Fixtures

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # As fixtures serve para nao precisarmos inserir os dados para insersao
+  # na hora de teste. Deve criar um arquivo chamado aqui em baixo 'customers na pasta fixtures'
+  fixtures :customers # ou insere all ao inves de customers
+
+  it 'Create a Customer' do
+    # subject.name = 'Douglas Kurotaki'
+    # subject.email = 'douglas.kurotaki@gmail.com'
+    # subject.save
+
+    customer = customers(:douglas)
+
+    expect(customer.full_name).to eq('Sr. Douglas Kurotaki')
+  end
 end
