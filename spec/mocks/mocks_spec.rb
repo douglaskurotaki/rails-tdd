6f61384fcd6d15468cc476012cf87a996dda466c
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2020-10-24 00:10:04 -0300

    Mock Expectations

--- spec/mocks/mocks_spec.rb
+++ spec/mocks/mocks_spec.rb
@@ -14,4 +14,27 @@ describe 'Mocks' do
     # exercise
     student.bar
   end
+
+  # Valida os argumentos
+  it 'args' do
+    student = Student.new
+    expect(student).to receive(:foo).with(123)
+    student.foo(123)
+  end
+
+  # Valida os argumentos e as repeticoes que devem ter
+  it 'repetição' do
+    student = Student.new
+    expect(student).to receive(:foo).with(123).twice #once, twice, exactly(3).times, at_leat(:once), at_least(:twice), at_leat(n).times
+    student.foo(123)
+    student.foo(123)
+  end
+
+  it 'retorno' do
+    student = Student.new
+    # Forca o retorno
+    expect(student).to receive(:foo).with(123).and_return(true)
+    # Dessa forma essa declaracao abaixa reotrno true
+    student.foo(123)
+  end
 end
