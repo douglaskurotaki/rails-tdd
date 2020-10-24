2e612e81840abb6c92dd92092b46866e378618d6
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2020-10-23 22:59:40 -0300

    Tag filters

--- spec/matchers/all/all_spec.rb
+++ spec/matchers/all/all_spec.rb
@@ -1,4 +1,4 @@
-describe 'all' do
+describe 'all', type: 'collection' do
   # O all serve para testar todos os elementos com alguma condicao
   # Podemos usar composicao tambem
   it { expect([1, 7, 9]).to all (be_odd).and be_an(Integer) }
--- spec/matchers/arrays/arrays_spec.rb
+++ spec/matchers/arrays/arrays_spec.rb
@@ -1,6 +1,10 @@
 RSpec::Matchers.define_negated_matcher :an_array_excluding, :include
 
-describe Array.new([1, 2, 3]), 'Array' do
+# collection: true serve para filtrar os testes no terminal
+# podemos fazer com chave e valor tambem, no caso ficaria
+# describe Array.new([1, 2, 3]), 'Array', type: 'collection' do
+# para executar: rspec . -t collection ou rspec . -t type: collection
+describe Array.new([1, 2, 3]), 'Array', collection: true do # ou somente collection
   # Verifica se contem um elemento ou mais no array
   it '#include' do
     expect(subject).to include(2)
@@ -11,7 +15,7 @@ describe Array.new([1, 2, 3]), 'Array' do
   it { expect(subject).to an_array_excluding(4) }
 
   # Elementos devem ser exatos, mas nao precisa ser na ordem
-  it '#match_array' do
+  it '#match_array', :slow do # --tag ~slow     serve para excluir essa tag. Configurado no .rspec
     expect(subject).to match_array([2, 1, 3])
   end
 
