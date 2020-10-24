69c26e88780de835120a03bf3aae9f8d86a8ebab
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2020-10-24 17:44:37 -0300

    Factorybot

---
+++ test_app/spec/factories/customer.rb
@@ -0,0 +1,7 @@
+# This will guess the User class
+FactoryBot.define do
+  factory :customer do
+    name { 'Douglas Kurotaki' }
+    email  { 'douglas.kurotaki@gmail.com' }
+  end
+end
--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -3,14 +3,18 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
   # As fixtures serve para nao precisarmos inserir os dados para insersao
   # na hora de teste. Deve criar um arquivo chamado aqui em baixo 'customers na pasta fixtures'
-  fixtures :customers # ou insere all ao inves de customers
+  # fixtures :customers # ou insere all ao inves de customers
 
   it 'Create a Customer' do
     # subject.name = 'Douglas Kurotaki'
     # subject.email = 'douglas.kurotaki@gmail.com'
     # subject.save
 
-    customer = customers(:douglas)
+    # customer = customers(:douglas)
+
+    # As fabricas servem justamente para termos esses dados de testes
+    # assim criando seu arquivo e sua chamada 'customer' factories/customer.rb
+    customer = create(:customer)
 
     expect(customer.full_name).to eq('Sr. Douglas Kurotaki')
   end
