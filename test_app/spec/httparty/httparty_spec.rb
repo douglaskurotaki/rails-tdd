d1003f7df3f24e44f0ad68e457b482b6f816783b
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2020-10-26 22:58:02 -0300

    VCR

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -3,15 +3,19 @@ describe 'HTTParty' do
   it 'content-type' do
     # Evita a consulta na internet para fazer o teste ser mais rapido
     # Faz com que simulemos uma consulta da api, mas eh so uma SIMULACAO
-    stub_request(:get, 'https://jsonplaceholder.typicode.com/posts/2').
-      to_return(status: 200, body: '', headers: { 'content-type': 'application/json' })
+    # stub_request(:get, 'https://jsonplaceholder.typicode.com/posts/2').
+    #   to_return(status: 200, body: '', headers: { 'content-type': 'application/json' })
 
-    # Serve para acessar um api externa e pegar os dados provenientes nela
-    # utilizamos gem httparty
-    # faz a requisicao e insere para o atributo
-    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
-    content_type = response.headers['content-type']
-    puts content_type
-    expect(content_type).to match(/application\/json/)
+    # O VCR faz com que haja a requisicao na internet, mas na proxima vez
+    # nao fara, pois ele vai gravar em um arquivo configurado no spec_rails
+    VCR.use_cassette('jsonplaceholder/posts') do
+      # Serve para acessar um api externa e pegar os dados provenientes nela
+      # utilizamos gem httparty
+      # faz a requisicao e insere para o atributo
+      response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+      content_type = response.headers['content-type']
+      puts content_type
+      expect(content_type).to match(/application\/json/)
+    end
   end
 end
