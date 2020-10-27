56b1ea5df880010b8c1ba0c201e51900e3e6a00d
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2020-10-26 21:55:18 -0300

    create_list

--- test_app/spec/models/order_spec.rb
+++ test_app/spec/models/order_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe Order, type: :model do
     order = create(:order)
     expect(order.customer).to be_kind_of(Customer)
   end
+
+  it 'Tem 3 pedidos' do
+    # Create List faz com que nao precisemos criar varias instancias
+    # basta inserir o numero de instancia no parametro
+    # Caso queira sobreescrever algum campo, basta inserir o nome dele e o valor
+    orders = create_list(:order, 3, description: 'Teste')
+    puts orders.inspect
+    expect(orders.count).to eq(3)
+  end
 end
