3c3b7d38f128236d664aab951fc3cfb4db277cc5
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2020-10-26 22:49:13 -0300

    webmock

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -1,7 +1,13 @@
 describe 'HTTParty' do
-  # Serve para acessar um api externa e pegar os dados provenientes nela
-  # utilizamos gem httparty
-  it 'Httparty' do
+
+  it 'content-type' do
+    # Evita a consulta na internet para fazer o teste ser mais rapido
+    # Faz com que simulemos uma consulta da api, mas eh so uma SIMULACAO
+    stub_request(:get, 'https://jsonplaceholder.typicode.com/posts/2').
+      to_return(status: 200, body: '', headers: { 'content-type': 'application/json' })
+
+    # Serve para acessar um api externa e pegar os dados provenientes nela
+    # utilizamos gem httparty
     # faz a requisicao e insere para o atributo
     response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
     content_type = response.headers['content-type']
