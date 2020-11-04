7715920dd48935091c09024e5894ef09011bc102
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2020-11-03 22:32:52 -0300

    config screenshot

--- test_app/spec/features/customers_spec.rb
+++ test_app/spec/features/customers_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 # Capybara para teste da view
-RSpec.feature "Customers", type: :feature do
+RSpec.feature "Customers", type: :feature, js: true do
   it 'Visit index page' do
     visit(customers_path)
     # printa todo html da pagina
     print page.html
+
+    # Tira um print da tela
+    page.save_screenshot('screenshot.png')
+
     # Verifica se a pagina atual foi a que visitou
     expect(page).to have_current_path(customers_path)
   end
