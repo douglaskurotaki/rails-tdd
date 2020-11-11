1e3a32906ffa0ba2b5dc50d664ada3dc9b826494
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2020-11-11 07:53:31 -0300

    Matcher include_json

--- test_app/spec/requests/customers_request_spec.rb
+++ test_app/spec/requests/customers_request_spec.rb
@@ -17,11 +17,11 @@ RSpec.describe CustomersController, type: :controller do
   end
    
   describe 'as Logged Member' do
-    it 'returns a 302 response(not authorized)' do
-      customer = create(:customer)
-      get :show, params: { id: customer.id }
-      expect(response).to have_http_status(302)
-    end
+    # it 'returns a 302 response(not authorized)' do
+    #   customer = create(:customer)
+    #   get :show, params: { id: customer.id }
+    #   expect(response).to have_http_status(302)
+    # end
 
     it 'render a :show template' do
       member = create(:member)
--- test_app/spec/requests/customers_spec.rb
+++ test_app/spec/requests/customers_spec.rb
@@ -3,8 +3,26 @@ require 'rails_helper'
 RSpec.describe "Customers", type: :request do
   describe "GET /customers" do
     it "works! (now write some real specs)" do
-      get customers_index_path
+      get customers_path
       expect(response).to have_http_status(200)
+    end
+
+    it "index - JSON 200 ok" do
+      get '/customers.json'
+      expect(response.body).to include_json(
+        [
+          id: 1,
+          name: 'Darla Carroll',
+          email: 'meu_email-1@email.com'
+        ]
+      )
+    end
+
+    it "show - JSON 200 ok" do
+      get '/customers/1.json'
+      expect(response.body).to include_json(
+          id: 1
+      )
     end
   end
 end
